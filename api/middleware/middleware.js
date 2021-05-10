ec31e46f8e5feafb5918b7da8b342a7e1560d881
matt22881 <matt22881@gmail.com>
2021-05-10 07:08:38 -0400

    Add files via upload

---
+++ api/middleware/middleware.js
@@ -0,0 +1,17 @@
+function logger(req, res, next) {
+  // DO YOUR MAGIC
+}
+
+function validateUserId(req, res, next) {
+  // DO YOUR MAGIC
+}
+
+function validateUser(req, res, next) {
+  // DO YOUR MAGIC
+}
+
+function validatePost(req, res, next) {
+  // DO YOUR MAGIC
+}
+
+// do not forget to expose these functions to other modules
